06e7125a9b72ea92db83c4de5e25a64fd9ffb1b7
sandibnayak <47876374+sandibnayak@users.noreply.github.com>
2021-11-24 09:44:11 +0530

    Input text changed for IOS sample app

--- src/main/java/BrowserStackAndroid.java
+++ src/main/java/BrowserStackAndroid.java
@@ -112,7 +112,7 @@ public class BrowserStackAndroid {
 
                 MobileElement inputField = driver.findElement(By.xpath("//XCUIElementTypeTextField[@name='Text Input']"));
                 wait.until(ExpectedConditions.visibilityOf(inputField));
-                inputField.sendKeys("IOS Test Text");
+                inputField.sendKeys("Test String 'ddd'");
             break;
 
 }
